91f9b6d85bb7c1f69613c20ecc81c03d580f8587
Engel <andrea.engel@sap.com>
2020-04-09 15:17:34 +0200

    Initialize repository with easy-ui5

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "shoppingassist/ShopNavigator/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("shoppingassist.ShopNavigator.controller.MainView", {});
+});

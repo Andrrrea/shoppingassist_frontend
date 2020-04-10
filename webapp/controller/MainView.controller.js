613973ea388ea2a0811896ad3110d6eaf42ccff3
Engel <andrea.engel@sap.com>
2020-04-10 14:13:29 +0200

    initial commit

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -3,5 +3,22 @@ sap.ui.define([
 ], function(Controller) {
   "use strict";
 
-  return Controller.extend("shoppingassist.ShopNavigator.controller.MainView", {});
+  return Controller.extend("shoppingassist.ShopNavigator.controller.MainView", {
+
+    onInit: function(){
+      var otable = this.getView().byId("productList");
+      var oModel = new sap.ui.model.json.JSONModel();
+      oModel.loadData("model/Products.json");
+      otable.setModel(oModel);
+    },
+
+    onCalculation: function(){
+      var otable = this.getView().byId("productList").getSelectedItems();
+      console.log(otable);
+    }
+
+    //AJAX call for backend (POST request)
+    //schicke liste mit selektierten Produkten
+
+  });
 });
